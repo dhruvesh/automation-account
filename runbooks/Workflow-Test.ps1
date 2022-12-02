55c2c052c37742a1ec2b6d7e1a36c1c99bc6346c
Dhruvesh Parikh <dhparikh@microsoft.com>
2022-12-02 11:15:26 +0530

    added work flow test

--- runbooks/Workflow-Test.ps1
+++ runbooks/Workflow-Test.ps1
@@ -1,4 +1,9 @@
-workflow workflow-test
-{
-    “Hello World”
-}
+Param(
+    [Parameter(mandatory = $true)]
+    [string] $resourceGroupName,
+
+    [Parameter(mandatory = $true)]
+    [string] $vmname
+)
+Connect-AzAccount -Identity
+Stop-AzVM -ResourceGroupName $resourceGroupName -Name $vmname -Force
